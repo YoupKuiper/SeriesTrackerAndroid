cecfbf509171aae83cf9b12f2ac0355d51cc761d
Youp <youpkuiper@gmail.com>
2018-10-19 20:27:45 +0200

    Working on Search

--- app/src/main/java/youp/seriestracker/activities/MainActivity.kt
+++ app/src/main/java/youp/seriestracker/activities/MainActivity.kt
@@ -1,9 +1,14 @@
 package youp.seriestracker.activities
 
+import android.app.Fragment
+import android.app.SearchManager
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,7 +23,12 @@ import youp.seriestracker.adapters.SeriesAdapter
 import youp.seriestracker.models.Body
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
+import youp.seriestracker.Fragments.HomeFragment
+import youp.seriestracker.Fragments.SearchFragment
+import youp.seriestracker.Fragments.SettingsFragment
 import youp.seriestracker.models.SeriesResponse
+import android.view.MenuInflater
+import android.widget.SearchView
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,52 +37,70 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
-        val retrofit = RetrofitClient.client
-        val service = retrofit.create(APIService::class.java)
+        val mainFragment = HomeFragment()
 
-        seriesList.onItemClickListener = object : OnItemClickListener {
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.add(R.id.container, mainFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
 
-            override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                val series = seriesList.getItemAtPosition(position)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.search, menu)
 
-                //Go to detail activity, pass clicked item
-                val intent = Intent(applicationContext, DetailActivity::class.java).apply {
-                    putExtra("SERIES", series as Series)
-                }
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
 
-                startActivity(intent)
-            }
-        }
-
-        val call = service.listSeries()
-        call.enqueue(object : Callback<SeriesResponse> {
-            override fun onResponse(call: Call<SeriesResponse>?, response: Response<SeriesResponse>?) {
-
-                if (response != null && response.isSuccessful) {
-                    seriesList.adapter = SeriesAdapter(applicationContext, response.body()?.series)
-                } else {
-                    Toast.makeText(applicationContext, "No Series Found", Toast.LENGTH_SHORT).show();
-                }
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(s: String): Boolean {
+                println(s)
+                searchView.clearFocus()
+                return true
             }
 
-            override fun onFailure(call: Call<SeriesResponse>?, t: Throwable?) {
-                Toast.makeText(applicationContext, t.toString(), Toast.LENGTH_LONG).show();
+            override fun onQueryTextChange(s: String): Boolean {
+                return false
             }
         })
+
+        return true
     }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-//                message.setText(R.string.title_home)
+                println("Home pressed")
+                val mainFragment = HomeFragment()
+
+                val manager = supportFragmentManager
+                val transaction = manager.beginTransaction()
+                transaction.replace(R.id.container, mainFragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-//                message.setText(R.string.title_dashboard)
+                println("Search pressed")
+                val searchFragment = SearchFragment()
+                val manager = supportFragmentManager
+                val transaction = manager.beginTransaction()
+                transaction.replace(R.id.container, searchFragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-//                message.setText(R.string.title_notifications)
+                println("Settings pressed")
+                val settingsFragment = SettingsFragment()
+                val manager = supportFragmentManager
+                val transaction = manager.beginTransaction()
+                transaction.replace(R.id.container, settingsFragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
                 return@OnNavigationItemSelectedListener true
             }
         }
